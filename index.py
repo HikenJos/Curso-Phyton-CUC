7607e8912610bdfb1d5b22c9f3362921c46bf383
Android Sala 15 <android@cuc.edu.co>
2019-04-27 10:41:52 -0500

    Operaciones y variables basicas

--- index.py
+++ index.py
@@ -4,5 +4,40 @@ Editor de Spyder
 
 Este es un archivo temporal.
 """
+# -*- Esto es un comentario lineal -*-
 
+""" Esto es un
+    Comentario de un parrafo """
+    
 a = 5
+# -*- VARIABLES -*-
+print("Hola Mundo")
+
+x = "TEXTO"
+z = 4.5
+a = True
+b = False
+c = 4 + 5j
+
+print(x, z, a, b, c)
+
+print(type(b))
+
+
+# -*- CONVERCIONES DE TIPO DE DATOS -*-
+
+x = int(2)
+print(x)
+
+x = int(2.8)
+print(x)
+
+# -*- FLOTAT(S) -*-
+
+x = float(2)
+print(x)
+
+# -*- CADENAS -*-
+
+x = str(2)
+print(x)
